f3fde44985547f67371225378f07fb21d4dcbbd3
Marcelo Elizeche Landó <melizeche@gmail.com>
2019-09-17 00:12:48 -0400

    refactor to use be able to use it as a CLI tool as a library and also aesthetic  changes

--- screen.py
+++ screen.py
@@ -1,34 +1,47 @@
 # Ubuntu: apt install chromium-chromedriver
 # Arch/Manjaro: pacman -S chromium 
-
-from selenium import webdriver
 import time
+from selenium import webdriver
 from datetime import datetime
 from PIL import Image
+from pathlib import Path
 
 URL = "https://www.airvisual.com/air-quality-map?lat=-23.60066&lng=-58.23591&zoomLevel=7"
 
-# Headless Chrome/Selenium setup
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
-options.add_argument("disable-infobars")  # disabling infobars
-options.add_argument("--disable-extensions")  # disabling extensions
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--no-sandbox")  # Bypass OS security model
-# Thanks to https://stackoverflow.com/a/50642913/2291648 for explaining the arguments above
+SCREENSHOTS_DIR = Path.cwd().joinpath('screenshots')
+if not SCREENSHOTS_DIR.exists():
+    SCREENSHOTS_DIR.mkdir()
 
-timestamp_str = datetime.now().strftime("%b%d-%H")
-screenshot_filename = f"screen_{timestamp_str}.png" 
+def get_screenshot():
+    # Headless Chrome/Selenium setup
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("disable-infobars")  # disabling infobars
+    options.add_argument("--disable-extensions")  # disabling extensions
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")  # Bypass OS security model
+    # Thanks to https://stackoverflow.com/a/50642913/2291648 for explaining the arguments above
 
-with webdriver.Chrome(options=options) as driver:
-    driver.set_window_size(1920,900)
-    driver.get(URL)
-    time.sleep(4)
-    driver.save_screenshot(screenshot_filename)
+    timestamp_str = datetime.now().strftime("%b%d-%H")
+    screenshot_path = SCREENSHOTS_DIR.joinpath(f"screen_{timestamp_str}.png")
+    print(screenshot_path)
+    try:
+        with webdriver.Chrome(options=options) as driver:
+            driver.set_window_size(1920,900)
+            driver.get(URL)
+            time.sleep(4)
+            driver.save_screenshot(screenshot_path._str)
 
-im = Image.open(screenshot_filename)
-#Set box coordinates to crop
-left, top, right, bottom = 542, 0, 1346, 900
-cropped_img = im.crop((left,top,right,bottom))
-cropped_img.save(screenshot_filename)
+        im = Image.open(screenshot_path)
+        # Set box coordinates to crop (in pixels)
+        left, top, right, bottom = 542, 0, 1346, 900
+        cropped_img = im.crop((left,top,right,bottom))
+        cropped_img.save(screenshot_path)
+    except Exception as e:
+        print(type(e),e)
+        return False
 
+    return True
+
+if __name__ == "__main__" :
+    get_screenshot()
